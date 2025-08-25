23dee48ebbfee02b416a8de4ffdef66bfef2ed60
Christiaan van Dam <chris@trace.nl>
2025-08-25 23:00:08 +0200

    add Doctrine and Symfony configuration to project files

--- tests/object-manager.php
+++ tests/object-manager.php
@@ -16,7 +16,7 @@ require dirname(__DIR__).'/vendor/autoload.php';
 
 (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$kernel = new Kernel((string) $_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $kernel->boot();
 
 return $kernel->getContainer()->get('doctrine')->getManager();
