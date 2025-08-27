4ba30b35ef5ab541c66366538d8cc307a4941430
Christiaan van Dam <chris@trace.nl>
2025-08-27 18:28:32 +0200

    Add `symfony/messenger` integration with adapters and event subscribers
    - Installed and configured `symfony/messenger` in the application.
    - Added `SymfonyEventDispatcherAdapter` for event dispatching using Symfony's event dispatcher.
    - Added `MessengerEventDispatcherAdapter` for message handling using Symfony Messenger.
    - Created `NodeEventsLogSubscriber` to log content repository events.
    - Updated `.env`, composer, and `.idea` configurations to support the messenger setup.

---
+++ src/Infrastructure/Event/MessengerEventDispatcherAdapter.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Aurora Core.
+ *
+ * (c) The Aurora Core contributors
+ * License: MIT
+ */
+
+namespace Aurora\Infrastructure\Event;
+
+use Aurora\Application\Event\EventDispatcher;
+use Aurora\Domain\Event\DomainEvent;
+use Symfony\Component\Messenger\Exception\ExceptionInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+class MessengerEventDispatcherAdapter implements EventDispatcher
+{
+    public function __construct(private readonly MessageBusInterface $bus)
+    {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws ExceptionInterface
+     */
+    public function dispatch(DomainEvent $event): void
+    {
+        $this->bus->dispatch($event);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws ExceptionInterface
+     */
+    public function dispatchAll(iterable $events): void
+    {
+        foreach ($events as $event) {
+            $this->dispatch($event);
+        }
+    }
+}
